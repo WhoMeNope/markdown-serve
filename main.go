a1e9e3c98ef4d1bad60893127809c2186104c19a
WhoMeNope <whomenope@outlook.com>
2018-09-10 19:27:46 +0200

    preserve paragraph structure

--- main.go
+++ main.go
@@ -1,13 +1,33 @@
 package main
 
 import (
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 
+	"github.com/gomarkdown/markdown/ast"
 	"github.com/gomarkdown/markdown/html"
 	"github.com/valyala/fasthttp"
 )
+
+func customRenderHook(w io.Writer, node ast.Node, entering bool) (ast.WalkStatus, bool) {
+	// skip all nodes that are not text nodes
+	if _, ok := node.(*ast.Text); !ok {
+		return ast.GoToNext, false
+	}
+
+	textNode := node.(*ast.Text)
+
+	// custom rendering logic for ast.Text
+	for _, v := range strings.Split(string(textNode.Leaf.Literal), "\n") {
+		w.Write([]byte(v + "</br>"))
+	}
+
+	// return (ast.GoToNext, true) to tell html renderer to skip rendering this node
+	return ast.GoToNext, true
+}
 
 func main() {
 	//load styles
@@ -20,9 +40,10 @@ func main() {
 	//setup renderer
 	htmlFlags := html.CommonFlags | html.CompletePage
 	opts := html.RendererOptions{
-		Title: "story tale",
-		CSS:   "/" + os.Getenv("STYLEFILE"),
-		Flags: htmlFlags,
+		Title:          "story tale",
+		CSS:            "/" + os.Getenv("STYLEFILE"),
+		Flags:          htmlFlags,
+		RenderNodeHook: customRenderHook,
 	}
 	renderer := html.NewRenderer(opts)
 
